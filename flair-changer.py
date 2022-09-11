0f173626803a4e73d667401e2ac8a6619fb1ec45
indiasocial <113117034+indiasocial@users.noreply.github.com>
2022-09-11 14:57:34 +0530

    v1.0
    created new flair changing bot for r/ISO

---
+++ flair-changer.py
@@ -0,0 +1,21 @@
+import praw
+
+reddit = praw.Reddit(
+    client_id="",
+    client_secret="",
+    password="",
+    user_agent="",
+    username="",
+)
+
+target_sub = "ISOCSS"
+subreddit = reddit.subreddit(target_sub)
+trigger_phrase = "!flair"
+
+for comment in subreddit.stream.comments():
+  
+    if trigger_phrase in comment.body:
+  
+        word = comment.body.replace(trigger_phrase, "")
+        author = comment.author
+        subreddit.flair.set(author, text=word)
